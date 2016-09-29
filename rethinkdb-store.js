a9df99cb450f9440130b54359b8933224d51ab37
mderrier <mderrier@users.noreply.github.com>
2016-09-29 14:26:24 +0200

    change to rethinkdbdash

--- rethinkdb-store.js
+++ rethinkdb-store.js
@@ -1,10 +1,10 @@
 'use strict'
 
 var _ = require('lodash')
-var r = require('rethinkdb')
-
+// var r = require('rethinkdb')
+var Promise = require('bluebird')
 var StandardQueryPlugin = require('seneca-standard-query')
-
+var r
 var storename = 'rethinkdb-store'
 
 module.exports = function (opts) {
@@ -23,7 +23,32 @@ module.exports = function (opts) {
   seneca.ready(function () {
     StandardQuery = seneca.export('standard-query/utils')
   })
+
   var internals = {}
+
+  // function error (query, args, err/*, next*/) {
+  //     if (err) {
+  //       var errorDetails = {
+  //         message: err.message,
+  //         err: err,
+  //         stack: err.stack,
+  //         query: query
+  //       }
+  //       seneca.log.error('Query Failed', JSON.stringify(errorDetails, null, 1))
+  //       // next ({code: 'entity/error', store: name})
+  //
+  //       if ('ECONNREFUSED' === err.code || 'notConnected' === err.message || 'Error: no open connections' === err) {
+  //         minwait = opts.minwait
+  //         if (minwait) {
+  //           reconnect(args)
+  //         }
+  //       }
+  //
+  //       return true
+  //     }
+  //
+  //     return false
+  //   }
 
   function configure (spec, cb) {
     if (!_.isUndefined(spec.connect) && !spec.connect) {
@@ -35,18 +60,33 @@ module.exports = function (opts) {
     var dbopts = seneca.util.deepextend({
       host: '127.0.0.1',
       port: 28015,
-      db: 'test'
+      db: 'test',
+      discovery: true,
+      pingInterval: 2
     }, conf)
+
+    var rethinkOpts = {
+      servers: [{host: dbopts.host, port: dbopts.port}],
+      db: dbopts.db,
+      discovery: true,
+      pingInterval: 2
+    }
     db = dbopts.db
-
-    r.connect(dbopts, function (err, connection) {
-      if (err)
-        return seneca.die('connect', err, conf)
-
-      conn = connection
-      seneca.log.debug('init', 'connect')
-      cb()
-    })
+    r = require('rethinkdbdash')(rethinkOpts)
+    // setImmediate(function () {
+    // process.nextTick(function () {
+    return cb()
+    // })
+    // r.connect(dbopts).then(function (connection) {
+    //   conn = connection
+    //   seneca.log.debug('init', 'connect')
+    //   setImmediate(function() {
+    //     return cb()
+    //   })
+    //
+    // }).catch(err => {
+    //   return seneca.die('connect', err, conf)
+    // })
   }
 
   function tablename (canon) {
@@ -59,30 +99,19 @@ module.exports = function (opts) {
     name: storename,
 
     close: function (args, cb) {
-      conn.close(function () {
-        cb()
-      })
+      return cb()
     },
 
     save: function (args, cb) {
       var ent = args.ent
-
       var create = (null == ent.id)
       // var create = null | ent.id
-
       var canon = ent.canon$({object: true})
       var zone = canon.zone
       var base = canon.base
       var name = canon.name
 
-      // var qent = args.qent;
-      // var q = args.q;
-
-      // var canon = qent.canon$({object: true});
       var table = tablename(canon)
-      // var name = canon.name;
-
-      // console.log('table', table)
 
       if (create) {
         if (ent.id$) {
@@ -91,7 +120,6 @@ module.exports = function (opts) {
           do_save(id)
         }
         else {
-          // console.log('Create : no id found')
           this.act(
             {role: 'basic', cmd: 'generate_id',
             name: name, base: base, zone: zone},
@@ -107,7 +135,7 @@ module.exports = function (opts) {
       }
 
       function do_save (id) {
-        // console.log("do_save", id)
+        // console.log('do_save', id)
         var rdent = ent.data$(true, 'string')
 
         if (id) {
@@ -116,94 +144,97 @@ module.exports = function (opts) {
 
         rdent.entity$ = ent.entity$
 
-        r.db(db).table(table).get(rdent.id).run(conn, function (err, result) {
-          if (err) return cb(err)
-
+        r.table(table).get(rdent.id).run().then(function (result) {
+        // r.db(db).table(table).get(rdent.id).run(conn).then(function (result) {
           if (!result)
             do_insert(rdent)
           else
             do_update(result, rdent)
-        })
+        }).catch(err => cb)
       }
 
       function do_update (prev, rdent) {
         var obj = seneca.util.deepextend(prev, rdent)
         obj = seneca.util.clean(obj)
-        r.db(db).table(table).get(rdent.id).update(obj, {returnChanges: true}).run(conn, function (err, result) {
+        r.table(table).get(rdent.id).update(obj, {returnChanges: true}).run().then(function (result) {
+        // r.db(db).table(table).get(rdent.id).update(obj, {returnChanges: true}).run(conn).then(function (result) {
           // console.log(result.changes)
-          if (err)
-            cb(err)
-          else if (result.replaced > 0)
-            cb(null, ent.make$(result.changes[0].new_val))
+          if (result && result.replaced > 0)
+            return cb(null, ent.make$(result.changes[0].new_val))
           else
-            cb(null, ent.make$(rdent))
-        })
+            return cb(null, ent.make$(rdent))
+        }).catch(err => cb)
       }
 
       function do_insert (rdent) {
         rdent = seneca.util.clean(rdent)
-        r.db(db).table(table).insert(rdent, {returnChanges: true}).run(conn, function (err, result) {
-          if (err) return cb(err)
-          cb(null, ent.make$(result.changes[0].new_val))
-        })
+        r.table(table).insert(rdent, {returnChanges: true}).run().then(function (result) {
+//        r.db(db).table(table).insert(rdent, {returnChanges: true}).run(conn).then(function (result) {
+          return cb(null, result ? ent.make$(result.changes[0].new_val) : null)
+        }).catch(err => cb)
       }
     },
 
     load: function (args, cb) {
-      // console.log('load')
       var qent = args.qent
       var q = args.q
 
       var canon = qent.canon$({object: true})
       var name = canon.name
       var table = tablename(canon)
-
-      const query = q.id ? r.db(db).table(table).get(id) : r.db(db).table(table).filter(q).limit(1)
-      query.run(conn, function (err, cursor) {
-        if (err) return cb(err)
-        if (cursor.toArray) {
-          cursor.toArray((err, results) => {
-            if (err) return cb(err)
-            cb(null, results.length ? qent.make$(results[0]) : null)
-          })
-        } else {
-          cb(null, cursor ? qent.make$(cursor) : null)
-        }
-      })
+      // var query = q.id ? r.db(db).table(table).get(q.id) : r.db(db).table(table).filter(q).limit(1)
+      // query.run(conn).then(function (cursor) {
+      var query = q.id ? r.table(table).get(q.id) : r.table(table).filter(q).limit(1)
+      query.run().then(function (results) {
+        if (_.isArray(results) === false)
+          results = [results]
+        return cb(null, results.length ? qent.make$(results[0]) : null)
+      }).catch(err => cb)
     },
 
     list: function (args, cb) {
-      // console.log('qent',args.qent)
+      // console.log('qent', args.qent)
       var qent = args.qent
       var q = args.q
 
       var canon = qent.canon$({object: true})
       var table = tablename(canon)
       var name = canon.name
-      var list = []
-      // console.log('canon',canon)
+      // console.log('q', q)
+      // console.log('canon', canon)
       // console.log('table', tablename(canon))
-
-      r.db(db).table(table).filter(q).run(conn, function (err, cursor) {
-        if (err) return cb(err)
-
-        cursor.eachAsync(function (row) {
-          // console.log('row',row)
-          // var attrs = internals.transformDBRowToJSObject(row)
-          var attrs = internals.transformDBRowToJSObject(row)
-          // console.log('attrs',attrs)
-          var ent = StandardQuery.makeent(qent, attrs)
-          // console.log(row)
-          // console.log('ent',ent)
-          list.push(qent.make$(attrs))
-
-          // list.push(qent.make$(row));
-        }, function (final) {
-            // the 'final' argument will only be defined when there is an error
-            // console.log('Final called with:', list);
-          cb(null, list)
-        })
-      })
+      r.table(table).filter(q).run().then(results => {
+        // var list = []
+        //  if (err) return cb(err)
+        if (results)
+          results = results.map(function (item) { return qent.make$(item) })
+        console.log(results)
+        return cb(null, results)
+        // if (cursor) {
+        //   cursor.eachAsync(function (row) {
+        //   // console.log('row',row)
+        //   // var attrs = internals.transformDBRowToJSObject(row)
+        //     var attrs = internals.transformDBRowToJSObject(row)
+        //   // console.log('attrs',attrs)
+        //     var ent = StandardQuery.makeent(qent, attrs)
+        //   // console.log(row)
+        //   // console.log('ent',ent)
+        //     list.push(qent.make$(attrs))
+        //
+        //   // list.push(qent.make$(row));
+        //   }, function (final) {
+        //     // the 'final' argument will only be defined when there is an error
+        //     // console.log('Final called with:', list);
+        //     cb(null, list)
+        //     list = []
+        //     return
+        //   })
+        // } else {
+        //   cb(null, list)
+        //   list = []
+        //   return
+        // }
+      }).catch(err => cb)
     },
 
     remove: function (args, cb) {
@@ -218,26 +249,35 @@ module.exports = function (opts) {
       var load = _.isUndefined(q.load$) ? true : q.load$ // default true
 
       if (all) {
-        r.db(db).table(table).delete({returnChanges: load}).run(conn, function (err, result) {
-          if (err) return cb(err)
-
-          cb(null, _.map(load ? result.changes : [], function (e) {
+        // r.db(db).table(table)
+        // .delete({returnChanges: load})
+        // .run(conn)
+        r.table(table)
+        .delete({returnChanges: load})
+        .run()
+        .then(function (result) {
+          return cb(null, _.map(load ? result.changes : [], function (e) {
             return qent.make$(e.old_val)
           }))
         })
+        .catch(err => cb)
       }
       else {
         var q_clean = _.omit(q, function (k) {
           return _.endsWith(k, '$')
         }) // Remove cruft
-
-        r.db(db).table(table).filter(q_clean).delete({returnChanges: load}).run(conn, function (err, result) {
-          if (err) return cb(err)
-
-          cb(null, _.map(load ? result.changes : [], function (e) {
+        // r.db(db).table(table).filter(q_clean)
+        // .delete({returnChanges: load})
+        // .run(conn)
+        r.table(table).filter(q_clean)
+        .delete({returnChanges: load})
+        .run()
+        .then(function (result) {
+          return cb(null, _.map(load ? result.changes : [], function (e) {
             return qent.make$(e.old_val)
           }))
         })
+        .catch(err => cb)
       }
     },
 
